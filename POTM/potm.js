35ae2737de556fd99cce36ca1488dc2935436ed0
Ezreal72 <151830122+Ezreal72@users.noreply.github.com>
2024-01-03 01:26:52 +0100

    r

--- POTM/potm.js
+++ POTM/potm.js
@@ -1,1 +1,10 @@
+const btn = document.querySelector('#btn');
+const img = document.querySelector('#img');
 
+btn.addEventListener('click', ()=>{
+    if(img.style.display == 'block') {
+        img.style.display = 'none';
+    } else {
+        img.style.display = 'block';
+    }
+});
